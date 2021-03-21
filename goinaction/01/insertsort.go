d0de050756d5fbd24ac0927b6b36ed7568b91a14
Andy Liu <andyliumathematics@163.com>
2021-03-21 18:42:07 +0800

    insertsort

--- goinaction/01/insertsort.go
+++ goinaction/01/insertsort.go
@@ -9,10 +9,16 @@ func main() {
 		tmp := arr[i]
 		for j := i-1; j >= 0; j-- {
 			if arr[j] >= tmp {
-				arr[j+1] = arr[j]
-				arr[j] = tmp
+				// arr[j+1] = arr[j]
+				// arr[j] = tmp
+				swap(&arr,j,j+1)
 			}
 		}
 	}
 	fmt.Println(arr)
+}
+func swap(arr *[12]int64,i int,j int){
+	tmp := arr[i]
+	arr[i]=arr[j]
+	arr[j] = tmp
 }
